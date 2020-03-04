c028f8dfb670c66dc2c95829284893e2f9cec266
Ferdaws Omari <ferdaws.hedayat-omari@ocsbstudent.ca>
2020-03-04 13:52:55 -0500

    added animating images

--- DrawingShapesFerdaws/main.lua
+++ DrawingShapesFerdaws/main.lua
@@ -8,6 +8,34 @@
 
 local vertices = {0,-150, 90,65, -130,65}
 local triangle = display.newPolygon(768, 192, vertices )
+local textSize = 50
 
 
 triangle:setFillColor(0.9,0.3,0.5)
+
+
+-- write triangle on screen. 
+triangleText = display.newText("Triangle", 0, 0, Arial, textSize)
+
+-- anchor the text and set its (x,y) position 
+triangleText.anchorX = 0
+triangleText.anchorY = 0
+triangleText.x = 700
+triangleText.y = 300
+
+-- set the colour of the newText
+triangleText:setTextColor(1, 1, 1)
+
+local verticesHexagon = {-30,30, -5,30, -30,23, 30,25 -10,25, -5,25, 30,23, 0,-30}
+local hexagon = display.newPolygon(256,192, verticesHexagon )
+local textSize = 50
+
+
+hexagon:setFillColor(0.9,0.3,0.5)
+
+-- anchor the text and set its (x,y) position 
+hexagonText.anchorX = 0
+hexagonText.anchorY = 0
+hexagonText.x = 700
+hexagonText.y = 300
+
