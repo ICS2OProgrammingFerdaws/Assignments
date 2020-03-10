07f82382af00fdd48bd5d5e8692b32ba120f6e69
Ferdaws Omari <61517186+s466250@users.noreply.github.com>
2020-03-10 15:58:54 -0400

    added

--- AnimatingImagesFerdaws/main.lua
+++ AnimatingImagesFerdaws/main.lua
@@ -1,7 +1,40 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
 
--- Your code here
+
+
+
+
+
+
+
+-- hide the status bar 
+display.setStatusBar(display.HiddenStatusBar)
+
+local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
+
+
+local snoopDogg = display.newImageRect("Images/snoop.png", 300, 300)
+local snoopDoggWidth = snoopDogg.width 
+local snoopDoggHeight = snoopDogg.height
+
+local eminem = display.newImageRect("Images/eminem.jpg", 400,400)
+local eminemWidth = eminem.width 
+local emniemHeight = eminem.height
+
+
+local tupac = display.newImageRect("Images/tupac.png", 300, 300)
+local tupacWidth = tupac.width 
+local tupacHeight = tupac.height 
+ 
+local alreadyTouchedsnoopDogg = false 
+local alreadyTouchedeminem = false
+local alreadyTouchedtupac = false
+
+snoopDogg.x = 400
+snoopDogg.y = 500
+
+eminem.x = 100
+eminem.y = 400
+
+
+tupac.x = 700
+tupac.y = 400
--- DrawingShapesFerdaws/main.lua
+++ DrawingShapesFerdaws/main.lua
@@ -1,10 +1,7 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Tittle:Drawing Shapes
+-- Name:Ferdaws
+-- Course:ICS20/3C
 
--- Your code here
 
 local vertices = {0,-150, 90,65, -130,65}
 local triangle = display.newPolygon(768, 192, vertices )
@@ -26,16 +23,55 @@ triangleText.y = 300
 -- set the colour of the newText
 triangleText:setTextColor(1, 1, 1)
 
-local verticesHexagon = {-30,30, -5,30, -30,23, 30,25 -10,25, -5,25, 30,23, 0,-30}
-local hexagon = display.newPolygon(256,192, verticesHexagon )
+local verticesPentagon = {80,-120, 120,-50, -60,150, -100,-150, 0,-150}
+local pentagon = display.newPolygon(170,192, verticesPentagon )
 local textSize = 50
 
 
-hexagon:setFillColor(0.9,0.3,0.5)
+pentagon:setFillColor(0.9,0.3,0.5)
 
--- anchor the text and set its (x,y) position 
+
+pentagonText = display.newText("pentagon", 0, 0, Arial,textSize)
+-- anchor the text and set its (x,y) position
+pentagonText.anchorX = 0
+pentagonText.anchorY = 0
+pentagonText.x = 265
+pentagonText.y = 192
+
+local verticesHexagon = {60,-120, 120,0, 0,120, -60,120, -120,0, -100,-120}
+local hexagon = display.newPolygon(170,585, verticesHexagon)
+local textSize = 50
+
+hexagon:setFillColor(0.50,0.85,0.20)
+ 
+hexagonText = display.newText("hexagon", 0, 0,Arial, textSize)
+
+
+
+-- anchor the text and set its (x,y) position
+
 hexagonText.anchorX = 0
 hexagonText.anchorY = 0
-hexagonText.x = 700
-hexagonText.y = 300
+hexagonText.x = 170
+hexagonText.y =700
+
+local verticesOctagon = {60,-120, 100,-60, 100,60, 60,120, -20,120, -100,60, -100,-60, -40,-120}
+local octagon = display.newPolygon(768,585, verticesOctagon)
+local textSize = 50
+
+octagon:setFillColor(0.63,0.79,0.32)
+
+OctagonText = display.newText("Octagon", 0, 0,Arial, textSize)
+
+
+
+-- anchor the text and set its (x,y) position
+OctagonText.anchorX = 0
+OctagonText.anchorY = 0
+OctagonText.x = 768
+OctagonText.y = 700
+
+
+
+
 
