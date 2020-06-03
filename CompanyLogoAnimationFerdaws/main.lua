c757cf3522bfde4aa2ad8acd995220d1d5986840
Ferdaws Omari <61517186+s466250@users.noreply.github.com>
2020-06-02 21:53:03 -0400

    added

--- CompanyLogoAnimationFerdaws/main.lua
+++ CompanyLogoAnimationFerdaws/main.lua
@@ -1,7 +1,24 @@
 -----------------------------------------------------------------------------------------
 --
 -- main.lua
---
+-- Created by: Ferdaws
+-- Date: MAy 25, 2020
+-- Description: This calls the splash screen of the app to load itself.
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- Hiding Status Bar
+-- HIDE THE STATUS BAR
+
+-----------------------------------------------------------------------------------------
+
+-- Use composer library
+local composer = require( "composer" )
+
+-----------------------------------------------------------------------------------------
+
+-- Go to the intro screen
+composer.gotoScene( "splash_screen" )
+
+--------------------------------------------------------------------------------------------------------
+-- Hide the Statusbar 
+display.setStatusBar(display.HiddenStatusBar)
---
+++ CompanyLogoAnimationFerdaws/splash_screen.lua
@@ -0,0 +1,164 @@
+-----------------------------------------------------------------------------------------
+--
+-- splash_screen.lua
+-- Created by: Ferdaws
+-- Date: May 25, 2020
+-- Description: This is the splash screen of the game. It displays the 
+-- company logo that...
+-----------------------------------------------------------------------------------------
+
+-- Use Composer Library
+local composer = require( "composer" )
+
+-- Name the Scene
+sceneName = "splash_screen"
+
+-----------------------------------------------------------------------------------------
+
+-- Create Scene Object
+local scene = composer.newScene( sceneName )
+
+----------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+-----------------------------------------------------------------------------------------
+ 
+-- The local variables for this scene
+local Intro
+local scrollXSpeed = 8
+local scrollYSpeed = -3
+local WhooshSounds = audio.loadSound("Sounds/Whoosh-boom.mp3")
+local WhooshSoundsChannel
+local background
+
+--------------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
+--------------------------------------------------------------------------------------------
+
+-- The function that moves the Intro across the screen
+local function moveIntro()
+    Intro.x = Intro.x + scrollXSpeed
+    Intro.y = Intro.y + scrollYSpeed
+end
+
+-- The function that will go to the main menu 
+local function gotoMainMenu()
+    composer.gotoScene( "main_menu" )
+end
+
+-----------------------------------------------------------------------------------------
+-- GLOBAL SCENE FUNCTIONS
+-----------------------------------------------------------------------------------------
+
+-- The function called when the screen doesn't exist
+function scene:create( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -- set the background
+    background = display.newImageRect("Images/Background.png", 1024,768)
+    background.anchorX = 0
+    background.anchorY = 0
+
+    -- Insert the Intro image
+    Intro = display.newImageRect("Images/Intro.png", 500, 600)
+
+    -- set the initial x and y position of the Intro
+    Intro.x = 100
+    Intro.y = display.contentHeight/2
+
+    -- Insert objects into the scene group in order to ONLY be associated with this scene
+    sceneGroup:insert( background)
+    sceneGroup:insert( Intro )
+
+end -- function scene:create( event )
+
+--------------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to appear on screen
+function scene:show( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -----------------------------------------------------------------------------------------
+
+    local phase = event.phase
+
+    -----------------------------------------------------------------------------------------
+
+    -- Called when the scene is still off screen (but is about to come on screen).
+    if ( phase == "will" ) then
+       
+    -----------------------------------------------------------------------------------------
+
+    elseif ( phase == "did" ) then
+        -- start the splash screen music
+        jungleSoundsChannel = audio.play( WhooshSounds )
+
+        -- Call the moveIntro function as soon as we enter the frame.
+        Runtime:addEventListener("enterFrame", moveIntro)
+
+        -- Go to the main menu screen after the given time.
+        timer.performWithDelay ( 3000, gotoMainMenu)          
+        
+    end
+
+end --function scene:show( event )
+
+-----------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to leave the screen
+function scene:hide( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+    local phase = event.phase
+
+    -----------------------------------------------------------------------------------------
+
+    -- Called when the scene is on screen (but is about to go off screen).
+    -- Insert code here to "pause" the scene.
+    -- Example: stop timers, stop animation, stop audio, etc.
+    if ( phase == "will" ) then  
+
+    -----------------------------------------------------------------------------------------
+
+    -- Called immediately after scene goes off screen.
+    elseif ( phase == "did" ) then
+        
+        -- stop the jungle sounds channel for this screen
+        audio.stop (WhooshSoundsChannel)
+    end
+
+end --function scene:hide( event )
+
+-----------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to be destroyed
+function scene:destroy( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -----------------------------------------------------------------------------------------
+
+
+    -- Called prior to the removal of scene's view ("sceneGroup").
+    -- Insert code here to clean up the scene.
+    -- Example: remove display objects, save state, etc.
+end -- function scene:destroy( event )
+
+-----------------------------------------------------------------------------------------
+-- EVENT LISTENERS
+-----------------------------------------------------------------------------------------
+
+-- Adding Event Listeners
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+-----------------------------------------------------------------------------------------
+
+return scene
